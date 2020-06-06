fbcd98deb5d953752e257067f2bb7c152352de13
Adelakin Adewumi <61943042+Adelakin-Adewumi@users.noreply.github.com>
2020-06-05 17:31:33 -0700

    Update Adewumi.js

--- scripts/Adewumi.js
+++ scripts/Adewumi.js
@@ -1,8 +1,8 @@
-Name = 'Adewumi Adelakin'
+const Name = 'Adewumi Adelakin'
 id = 'HNG-04266'
 email = 'adelakinadewumi@gmail.com'
 language = 'Javascript'
 
-const output= 'Hello World, this is Adewumi Adelakin with HNG id 04266 and email address adelakinadewumi@gmail.com using Javascript for stage 2 task'
+console.log ('Hello World, this is Adewumi Adelakin with HNG id 04266 and email address adelakinadewumi@gmail.com using Javascript for stage 2 task')
 
 
